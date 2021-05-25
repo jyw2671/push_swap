b08c165af6c3ae403708edda9ed6ce26ac87c5d8
jyw2671 <yjung@student.42seoul.kr>
2021-05-25 12:40:39 +0900

    fix : gnl (linked_list) -> (char)

--- libft/include/get_next_line.h
+++ libft/include/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: yjung <yjung@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 12:03:13 by yjung             #+#    #+#             */
-/*   Updated: 2021/05/25 12:03:15 by yjung            ###   ########.fr       */
+/*   Updated: 2021/05/25 12:39:22 by yjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,6 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
 # endif
-
-typedef struct s_gnl
-{
-	int			fd;
-	char		*line;
-	void		*next;
-}				t_gnl;
 
 int		get_next_line(int fd, char **line);
 
--- libft/src/get_next_line/get_next_line.c
+++ libft/src/get_next_line/get_next_line.c
@@ -6,113 +6,71 @@
 /*   By: yjung <yjung@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 12:04:45 by yjung             #+#    #+#             */
-/*   Updated: 2021/05/25 12:04:46 by yjung            ###   ########.fr       */
+/*   Updated: 2021/05/25 12:39:18 by yjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static t_gnl	*gnl_new(int fd)
+#include "get_next_line.h"
+
+static int	get_next_line_make(char **store, char *buf, ssize_t read_size)
 {
-	t_gnl	*result;
+	char	*ret;
 
-	result = malloc(sizeof(t_gnl));
-	if (result == NULL)
-		return (0);
-	result->fd = fd;
-	result->line = 0;
-	result->next = 0;
-	return (result);
-}
-
-static t_gnl	*check_gnl(t_gnl **store, int fd)
-{
-	t_gnl	*ptr;
-
-	if (!(*store))
-		return ((*store) = gnl_new(fd));
-	ptr = *store;
-	while (ptr->next && ptr->fd != fd)
-		ptr = ptr->next;
-	if (ptr->fd == fd)
-		return (ptr);
-	return ((ptr->next) = gnl_new(fd));
-}
-
-static void	gnl_del(t_gnl **store, int fd)
-{
-	t_gnl	*prev;
-	t_gnl	*curr;
-
-	if (!((*store)->next))
+	buf[read_size] = '\0';
+	if (*store)
 	{
+		ret = ft_strjoin(*store, buf);
 		free(*store);
-		*store = 0;
-		return ;
 	}
-	prev = 0;
-	curr = *store;
-	while (curr->next && curr->fd != fd)
-	{
-		prev = curr;
-		curr = prev->next;
-	}
-	if (prev && curr->next)
-		prev->next = curr->next;
-	else if (!prev && curr->next)
-		*store = curr->next;
 	else
-		prev->next = 0;
-	free(curr);
+		ret = ft_strndup(buf, read_size);
+	*store = ret;
+	if (ft_strchr(*store, '\n'))
+		return (1);
+	return (0);
 }
 
-static int	ft_check_line(t_gnl **store, t_gnl *gnl, char **line)
+static int	get_next_line_check(char **store, char **line)
 {
-	char	*ptr;
 	char	*tmp;
+	char	*ptr;
 
-	ptr = ft_strchr(gnl->line, '\n');
+	ptr = ft_strchr(*store, '\n');
 	if (ptr)
 	{
-		*line = ft_strndup(gnl->line, ptr - gnl->line);
+		*line = ft_strndup(*store, ptr - *store);
 		tmp = ft_strndup(ptr + 1, ft_strlen(ptr + 1));
-		free(gnl->line);
-		gnl->line = tmp;
+		free(*store);
+		*store = tmp;
 		return (1);
 	}
 	else
 	{
-		*line = gnl->line;
-		gnl_del(store, gnl->fd);
+		*line = *store;
+		*store = 0;
 		return (0);
 	}
 }
 
 int	get_next_line(int fd, char **line)
 {
-	static t_gnl	*store;
+	static char		*store[OPEN_MAX];
+	char			*buf[BUFFER_SIZE + 1];
 	ssize_t			read_size;
-	t_gnl			*ptr;
-	char			buf[BUFFER_SIZE + 1];
 
-	if (fd < 0 || !line || BUFFER_SIZE <= 0)
-		return (-1);
-	ptr = check_gnl(&store, fd);
-	if (ptr == NULL)
+	if (!line || fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0)
 		return (-1);
 	read_size = read(fd, buf, BUFFER_SIZE);
 	while (read_size >= 0)
 	{
-		buf[read_size] = 0;
-		if (ptr->line)
-			ptr->line = ft_strjoin_free(ptr->line, buf, 1);
-		else
-			ptr->line = ft_strndup(buf, read_size);
-		if (ft_strchr(ptr->line, '\n') || read_size == 0)
+		if (get_next_line_make(&store[fd], (char *)buf, read_size) || \
+		read_size == 0)
 			break ;
 		read_size = read(fd, buf, BUFFER_SIZE);
 	}
 	if (read_size < 0)
 		return (-1);
-	return (ft_check_line(&store, ptr, line));
+	return (get_next_line_check(&store[fd], line));
 }

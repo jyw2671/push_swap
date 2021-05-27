35b11097ed409cabce0e96b9d74a9c5a7255d812
jyw2671 <yjung@student.42seoul.kr>
2021-05-27 14:18:12 +0900

    fix : stack_delone func 사용시 주솟값 저장 문제 해결

--- src/operation/rotate_a.c
+++ src/operation/rotate_a.c
@@ -6,7 +6,7 @@
 /*   By: yjung <yjung@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 20:58:21 by yjung             #+#    #+#             */
-/*   Updated: 2021/05/25 21:38:38 by yjung            ###   ########.fr       */
+/*   Updated: 2021/05/27 14:16:12 by yjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ int	rotate_a(t_stack **a, t_info *info)
 		return (1);
 	find_first_stack(a);
 	num = (*a)->value;
+	tmp = (*a)->bottom;
 	stack_delone(a);
+	*a = tmp;
 	if (!stack_add_back(a, num))
 		return (0);
 	return (1);
--- src/operation/rotate_b.c
+++ src/operation/rotate_b.c
@@ -6,7 +6,7 @@
 /*   By: yjung <yjung@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 21:35:08 by yjung             #+#    #+#             */
-/*   Updated: 2021/05/25 21:35:32 by yjung            ###   ########.fr       */
+/*   Updated: 2021/05/27 14:17:22 by yjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ int	rotate_b(t_stack **b, t_info *info)
 		return (1);
 	find_first_stack(b);
 	num = (*b)->value;
+	tmp = (*b)->bottom;
 	stack_delone(b);
+	*b = tmp;
 	if (!stack_add_back(b, num))
 		return (0);
 	return (1);
